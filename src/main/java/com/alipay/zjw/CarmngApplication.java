5b0031f03feee01a8f06f6abe73c10b65b91c5aa
zjwben <zjwben@gmail.com>
2016-02-15 23:49:37 +0800

    web app initialize files

---
+++ src/main/java/com/alipay/zjw/CarmngApplication.java
@@ -0,0 +1,12 @@
+package com.alipay.zjw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarmngApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarmngApplication.class, args);
+    }
+}
